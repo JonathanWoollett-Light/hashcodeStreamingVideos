483dc54be0f8c5a339a6af3b0e61a355b357728a
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-02-15 21:19:58 +0000

    Minor change

--- hashcodeStreamingVideos/src/hashcodeStreamingVideos/Program.java
+++ hashcodeStreamingVideos/src/hashcodeStreamingVideos/Program.java
@@ -1,9 +1,7 @@
 package hashcodeStreamingVideos;
 
-import java.io.*;
-
 public class Program {
-	public static void main(String args[]) throws IOException{
+	public static void main(String args[]) {
 		
 	}
 }
