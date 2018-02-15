f8debda1b6dad9a8c8fef2e9ff85fb01ed5e51e0
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-02-15 21:42:51 +0000

    Added videoSizes ArrayList

--- hashcodeStreamingVideos/src/hashcodeStreamingVideos/Program.java
+++ hashcodeStreamingVideos/src/hashcodeStreamingVideos/Program.java
@@ -1,7 +1,9 @@
 package hashcodeStreamingVideos;
 
+import java.util.ArrayList;
+
 public class Program {
+	private static ArrayList<Integer> videoSizes = new ArrayList<Integer>();
 	public static void main(String args[]) {
-		
 	}
 }
