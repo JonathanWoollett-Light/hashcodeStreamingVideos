607f408d4ed7daf740470ef550c0bbd395dedc77
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-02-15 21:34:17 +0000

    Added Request class

--- hashcodeStreamingVideos/src/hashcodeStreamingVideos/Endpoint.java
+++ hashcodeStreamingVideos/src/hashcodeStreamingVideos/Endpoint.java
@@ -3,10 +3,12 @@ package hashcodeStreamingVideos;
 public class Endpoint {
 	private final Connection connections[];
 	private final int latencyToDataCentre;
+	private final Request requests;
 	
-	public Endpoint(Connection[] connections, int latencyToDataCentre) {
+	public Endpoint(Connection[] connections, int latencyToDataCentre, Request requests) {
 		this.connections = connections.clone();
 		this.latencyToDataCentre = latencyToDataCentre;
+		this.requests = requests;
 	}
 	
 	public Connection[] getConnections() {
---
+++ hashcodeStreamingVideos/src/hashcodeStreamingVideos/Request.java
@@ -0,0 +1,11 @@
+package hashcodeStreamingVideos;
+
+public class Request {
+	private int number;
+	private int video;
+	
+	public Request(int number, int video) {
+		this.number = number;
+		this.video = video;
+	}
+}
