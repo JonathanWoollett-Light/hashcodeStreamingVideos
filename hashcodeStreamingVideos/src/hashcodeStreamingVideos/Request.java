dee7b7b9096c626b9acdfaa41a4b9a81a9daf045
JonathanWoollett-Light <jonathanwoollettlight@gmail.com>
2018-02-15 21:52:13 +0000

    Added get methods to Request class

--- hashcodeStreamingVideos/src/hashcodeStreamingVideos/Request.java
+++ hashcodeStreamingVideos/src/hashcodeStreamingVideos/Request.java
@@ -4,8 +4,15 @@ public class Request {
 	private int number;
 	private int video;
 	
-	public Request(int number, int video) {
+	public Request(int number, int video, int videoSize) {
 		this.number = number;
 		this.video = video;
+	}
+	
+	public int getNumber() {
+		return number;
+	}
+	public int getVideo() {
+		return video;
 	}
 }
